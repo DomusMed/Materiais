16389141cd897853b1d072a2559ef82f0f842ddd
DomusMed <contato@domusmed.site>
2025-09-04 18:22:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -27,15 +27,6 @@ headers.forEach((header, index) => {
   lista.appendChild(li);
 });
 
-// Sidebar toggle
-const toggleBtn = document.getElementById("toggle-btn");
-const sidebar = document.getElementById("sidebar");
-
-toggleBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
-  document.body.classList.toggle("sidebar-open");
-});
-
 // ============================
 // Barra de Progresso
 // ============================
